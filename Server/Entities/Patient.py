410759c19bbeb7111bd4138f37297d0479e432be
Antonio Huesa Guardiola <ahuesag@gmail.com>
2026-04-23 19:05:28 +0200

    inicializado la app servidor

--- Server/Entities/Patient.py
+++
@@ -1,9 +0,0 @@
-class Patient:
-    name = ""
-    idCard = ""
-    idHouse = -1
-
-    def __init__(self, name, idCard, idHouse):
-        self.name = name
-        self.idCard = idCard
-        self.idHouse = idHouse
